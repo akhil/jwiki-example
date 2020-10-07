7ebdcb02ab7af281046745be67d89af6830c4ce5
Akhil Kodali <50161+akhil@users.noreply.github.com>
2020-10-07 18:24:37 -0400

    added stub

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,2 +1,16 @@
+import okhttp3.HttpUrl;
+import org.fastily.jwiki.core.Wiki;
+
 public class Example {
+
+    public static void main(String[] s) {
+        Wiki wiki = new Wiki.Builder()
+                .withApiEndpoint(
+                        HttpUrl.parse("https://hinduismpedia.org/api.php")
+                )
+                .withLogin("", "")
+                .build();
+
+        wiki.edit("title", "text", "reason");
+    }
 }
